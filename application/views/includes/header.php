f9b59565555429aa64bb95dccf49cca0dcb72e34
jordyan <stevenjordyan@ymail.com>
2015-05-07 22:01:47 +0700

    initialize
    commit template front end

---
+++ application/views/includes/header.php
@@ -0,0 +1,11 @@
+<div id="header">
+    <div id="menu" class="container">
+        <ul>
+            <li class="current_page_item"><a href="#" accesskey="1" title="">Home</a></li>
+            <li><a href="#" accesskey="1" title="">Registrasi</a></li>
+            <li><a href="#" accesskey="2" title="">Konsultasi</a></li>
+            <li><a href="#" accesskey="3" title="">About Us</a></li>
+            <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+        </ul>
+    </div>
+</div>
